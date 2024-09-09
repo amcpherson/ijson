57b3b317287b1342c0f05a174efa90bc968b9b24
Rodrigo Tobar <rtobar@icrar.org>
2024-09-09 23:49:27 +0800

    Enable automatic backend and adaptor fixtures
    We have historically dynamically used a mixture of inheritance and
    module-level global definitions to test all backends using the different
    API flavours (file, async file, etc). This had led to hard-to-track
    logic with a weird code layout, where a test module defines a get_all
    function, which then gets solidified into a big class that is injected
    from code in test_base.
    
    This commit sets up the infrastructure to organise tests and
    dependencies more explicitly and less confusingly. Firstly, we now first
    list all backends and cache them globally. Secondly, a new class has
    been introduced that serves as the glue between a particular backend and
    a particular input type, and again we cache all of these.  Finally, the
    backend or adaptor (but never both) are now automatically injected into
    any function requesting a "backend" or "adaptor" fixture. This mechanism
    makes it very simple to write a new test function without having to do
    crazy tricks, or having to constrain ourselves to define it inside a
    single, common base class.
    
    Some existing tests only run against particular input types (mostly
    file-like objects), and thus we have also introduced a new marker that
    denotes such tests, and is used to decide which adaptors to parametrise
    functions with.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import enum
+
 import ijson
 
 import pytest
@@ -16,6 +18,81 @@ def _get_available_backends():
 _available_backends = _get_available_backends()
 
 
+class InputType(enum.Enum):
+    ASYNC_FILE = enum.auto()
+    ASYNC_TYPES_COROUTINES_FILE = enum.auto()
+    FILE = enum.auto()
+    SENDABLE = enum.auto()
+
+
+class BackendAdaptor:
+    """
+    Ties a backend together with an input type to provide easy access to
+    calling the backend's methods and retrieving all results in a single call.
+    """
+    def __init__(self, backend, input_type, suffix, get_all):
+        self.backend = backend
+        self._input_type = input_type
+        self._suffix = suffix
+        self._get_all = get_all
+
+    @property
+    def pytest_parameter_id(self):
+        return f"{self.backend.backend_name}-{self._input_type.name.lower()}"
+
+    def __getattr__(self, name):
+        routine = getattr(self.backend, name + self._suffix)
+        def get_all_for_name(*args, **kwargs):
+            return self._get_all(routine, *args, **kwargs)
+        return get_all_for_name
+
+
+from .test_async import get_all as get_all_async
+from .test_async_types_coroutines import get_all as get_all_async_types_coroutines
+from .test_coroutines import get_all as get_all_coro
+from .test_generators import get_all as get_all_gen
+
+_pull_backend_adaptors = [
+    backend_adaptor
+    for backend in _available_backends
+    for backend_adaptor in [
+        BackendAdaptor(backend, InputType.ASYNC_FILE, "_async", get_all_async),
+        BackendAdaptor(backend, InputType.ASYNC_TYPES_COROUTINES_FILE, "_async", get_all_async_types_coroutines),
+        BackendAdaptor(backend, InputType.FILE, "_gen", get_all_gen),
+    ]
+]
+
+_push_backend_adaptors = [
+    backend_adaptor
+    for backend in _available_backends
+    for backend_adaptor in [
+        BackendAdaptor(backend, InputType.SENDABLE, "_coro", get_all_coro),
+    ]
+]
+
+_all_backend_adaptors = _pull_backend_adaptors + _push_backend_adaptors
+
+BACKEND_PARAM_NAME = "backend"
+ADAPTOR_PARAM_NAME = "adaptor"
+
 def pytest_generate_tests(metafunc):
-    if "backend" in metafunc.fixturenames:
-        metafunc.parametrize("backend", _available_backends)
+    requires_backend = BACKEND_PARAM_NAME in metafunc.fixturenames
+    requires_adaptor = ADAPTOR_PARAM_NAME in metafunc.fixturenames
+    assert not (requires_backend and requires_adaptor)
+
+    names = []
+
+    # if both are required we need to match backend and adaptors correctly
+    if requires_backend:
+        names = BACKEND_PARAM_NAME
+        values = _available_backends
+        ids = [backend.backend_name for backend in _available_backends]
+    elif requires_adaptor:
+        pull_only = bool(list(metafunc.definition.iter_markers('pull_only')))
+        adaptors = _pull_backend_adaptors if pull_only else _all_backend_adaptors
+        names = ADAPTOR_PARAM_NAME
+        values = adaptors
+        ids = [adaptor.pytest_parameter_id for adaptor in adaptors]
+
+    if names:
+        metafunc.parametrize(names, values, ids=ids)
